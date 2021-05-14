8f5c2ae9457a0e53af701c72312628ae1e606453
Tim Tyrrell <timomitchel23@gmail.com>
2021-05-14 08:05:34 -0600

    Add second test

--- spec/features/search_congress_member_spec.rb
+++ spec/features/search_congress_member_spec.rb
@@ -12,5 +12,16 @@ RSpec.describe 'Govt Search' do
       expect(page).to have_content("Senator Bernard Sanders was found!")
       expect(page).to have_content("SenSanders")
     end
+
+    it 'allows user to search for another govt member' do
+      visit root_path
+
+      fill_in :search, with: 'Booker'
+      click_button 'Search'
+
+      expect(page.status_code).to eq 200
+      expect(page).to have_content("Senator Cory Booker was found!")
+      expect(page).to have_content("SenBooker")
+    end
   end
 end
